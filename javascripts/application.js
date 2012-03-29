613fe501320bf4a3b87a90b2fed40feeeb5c5189
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-03-28 23:54:20 -0400

    moving bubbles. NOT final movement

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,10 +1,32 @@
 $(function(){
   // ready function
   
+  /*
   $('.bubble').each(function(ind, obj){
     $(obj)
       .css("top", Math.random() * 400 )
       .css("left", Math.random() * 350 );
   });
+  */
+  
+  $('.bubble').each(function(ind, obj){
+    $(obj)
+      .css("top", (ind - ind % 3 )* 70 + 30 + Math.random() * 20 )
+      .css("left", ind % 3 * 185 + 10 );
+  });
+  
+  setInterval(function(){
+    $('.bubble').each(function(ind, obj){
+      if(Math.random() > .2){
+        $(obj)
+          .css("top", parseInt($(obj).css("top")) - Math.random() * 4 )
+          .css("left", parseInt($(obj).css("left")) - Math.random() * 4 ); 
+      } else if(Math.random() > .5) {
+        $(obj)
+          .css("top", parseInt($(obj).css("top")) + Math.random() * 4 )
+          .css("left", parseInt($(obj).css("left")) + Math.random() * 4 ); 
+      } 
+    });
+  }, 2000);
     
 });
