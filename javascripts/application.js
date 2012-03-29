4846c7fda4a5979cc2595bdf367aef37fd26bbaf
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-03-28 20:02:44 -0400

    basic layout

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,5 +1,10 @@
 $(function(){
   // ready function
   
-  
+  $('.bubble').each(function(ind, obj){
+    $(obj)
+      .css("top", Math.random() * 400 )
+      .css("left", Math.random() * 350 );
+  });
+    
 });
